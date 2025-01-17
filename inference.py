5b4aeeecdcf850119ec1f77622c670bd3bedd65e
Marco Busch <63476215+buma13@users.noreply.github.com>
2025-01-18 00:31:18 +0100

    cleanup inference

--- inference.py
+++ inference.py
@@ -42,32 +42,20 @@ def vis_pre(flow_pre, vis_dir):
 
     N = flow_pre.shape[0]
 
+    # Forward Flow
     for idx in range(N//2):
         flow_img = flow_viz.flow_to_image(flow_pre[idx].permute(1, 2, 0).numpy())
         image = Image.fromarray(flow_img)
         image.save('{}/flow_{:04}_to_{:04}.png'.format(vis_dir, idx+2, idx+3))
     
+    # Backward Flow
     for idx in range(N//2, N):
         flow_img = flow_viz.flow_to_image(flow_pre[idx].permute(1, 2, 0).numpy())
         image = Image.fromarray(flow_img)
         image.save('{}/flow_{:04}_to_{:04}.png'.format(vis_dir, idx-N//2+2, idx-N//2+1))
 
 @torch.no_grad()
-def MOF_inference(model, cfg):
-
-    model.eval()
-
-    input_images = prepare_image(cfg.seq_dir)
-    input_images = input_images[None].cuda()
-    padder = InputPadder(input_images.shape)
-    input_images = padder.pad(input_images)
-    flow_pre, _ = model(input_images, {})
-    flow_pre = padder.unpad(flow_pre[0]).cpu()
-
-    return flow_pre
-
-@torch.no_grad()
-def BOF_inference(model, cfg):
+def inference(model, cfg):
 
     model.eval()
 
@@ -108,15 +96,6 @@ if __name__ == '__main__':
     print(cfg.model)
     print("Parameter Count: %d" % count_parameters(model))
     
-    with torch.no_grad():
-        if args.mode == 'MOF':
-            from configs.multiframes_sintel_submission import get_cfg
-            flow_pre = MOF_inference(model.module, cfg)
-        elif args.mode == 'BOF':
-            from configs.sintel_submission import get_cfg
-            flow_pre = BOF_inference(model.module, cfg)
-    
+    flow_pre = inference(model.module, cfg)
+            
     vis_pre(flow_pre, cfg.vis_dir)
-
-
-

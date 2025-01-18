0c96d8b5ea31eecb264b69d97cb59c453f04b85f
Marco Busch <63476215+buma13@users.noreply.github.com>
2025-01-18 01:30:37 +0100

    add option to resize

--- inference.py
+++ inference.py
@@ -19,16 +19,22 @@ from utils.utils import InputPadder, forward_interpolate
 import itertools
 import imageio
 
-def prepare_image(seq_dir):
+def prepare_image(cfg, resize_factor = 1):
     print(f"preparing image...")
-    print(f"Input image sequence dir = {seq_dir}")
+    print(f"Input image sequence dir = {cfg.seq_dir}")
 
+    image_list = sorted(os.listdir(cfg.seq_dir))
     images = []
-
-    image_list = sorted(os.listdir(seq_dir))
-
+    run_once = False
     for fn in image_list:
-        img = Image.open(os.path.join(seq_dir, fn))
+        img = Image.open(os.path.join(cfg.seq_dir, fn))
+        if not run_once:
+            height = img.height // resize_factor
+            width = img.width // resize_factor
+            assert cfg.image_size[0] == height and cfg.image_size[1] == width,\
+            'Image dimensions ({},{}) not matching config ({},{})'.format(height, width, cfg.image_size[0], cfg.image_size[1])
+            run_once = True 
+        img = img.resize((width, height), resample=Image.Resampling.BOX)
         img = np.array(img).astype(np.uint8)[..., :3]
         img = torch.from_numpy(img).permute(2, 0, 1).float()
         images.append(img)
@@ -55,11 +61,11 @@ def vis_pre(flow_pre, vis_dir):
         image.save('{}/flow_{:04}_to_{:04}.png'.format(vis_dir, idx-N//2+2, idx-N//2+1))
 
 @torch.no_grad()
-def inference(model, cfg):
+def inference(model, cfg, resize_factor = 1):
 
     model.eval()
 
-    input_images = prepare_image(cfg.seq_dir)
+    input_images = prepare_image(cfg, resize_factor)
     input_images = input_images[None].cuda()
     padder = InputPadder(input_images.shape)
     input_images = padder.pad(input_images)
@@ -76,6 +82,7 @@ if __name__ == '__main__':
     parser.add_argument('--mode', default='MOF')
     parser.add_argument('--seq_dir', default='default')
     parser.add_argument('--vis_dir', default='default')
+    parser.add_argument('-r', '--resize_factor', type=int, default=1, help='Image resolution down sampling factor')
     
     args = parser.parse_args()
 
@@ -96,6 +103,6 @@ if __name__ == '__main__':
     print(cfg.model)
     print("Parameter Count: %d" % count_parameters(model))
     
-    flow_pre = inference(model.module, cfg)
+    flow_pre = inference(model.module, cfg, args.resize_factor)
             
     vis_pre(flow_pre, cfg.vis_dir)

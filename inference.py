86edd411e1c1c949f78831031477fc75457eec60
Marco Busch <63476215+buma13@users.noreply.github.com>
2025-01-18 14:57:58 +0100

    rescale out images back

--- inference.py
+++ inference.py
@@ -12,29 +12,40 @@ from core.Networks import build_network
 
 from utils.utils import InputPadder
 
-def prepare_image(cfg, resize_factor = 1):
+def prepare_resizing(cfg, resize_factor = 1):
+    image_list = sorted(os.listdir(cfg.seq_dir))
+    img = Image.open(os.path.join(cfg.seq_dir, image_list[0]))
+    
+    resolution_original = {
+        'height' : img.height,
+        'width' : img.width
+        }
+    
+    cfg.image_size[0] = img.height // resize_factor
+    cfg.image_size[1] = img.width // resize_factor
+
+    return resolution_original
+
+def prepare_image(cfg):
     print(f"preparing image...")
     print(f"Input image sequence dir = {cfg.seq_dir}")
 
     image_list = sorted(os.listdir(cfg.seq_dir))
     images = []
-    run_once = False
     for fn in image_list:
         img = Image.open(os.path.join(cfg.seq_dir, fn))
-        if not run_once:
-            height = img.height // resize_factor
-            width = img.width // resize_factor
-            assert cfg.image_size[0] == height and cfg.image_size[1] == width,\
-            'Image dimensions ({},{}) not matching config ({},{})'.format(height, width, cfg.image_size[0], cfg.image_size[1])
-            run_once = True 
-        img = img.resize((width, height), resample=Image.Resampling.BOX)
+        img = img.resize((cfg.image_size[1], cfg.image_size[0]), resample=Image.Resampling.BOX)
         img = np.array(img).astype(np.uint8)[..., :3]
         img = torch.from_numpy(img).permute(2, 0, 1).float()
         images.append(img)
+        
+    images.insert(0, images[0])
+    images.insert(0, images[0])
+    images.insert(0, images[0])
     
     return torch.stack(images)
 
-def vis_pre(flow_pre, vis_dir):
+def vis_pre(flow_pre, vis_dir, original_resolution):
 
     if not os.path.exists(vis_dir):
         os.makedirs(vis_dir)
@@ -45,20 +56,22 @@ def vis_pre(flow_pre, vis_dir):
     for idx in range(N//2):
         flow_img = flow_viz.flow_to_image(flow_pre[idx].permute(1, 2, 0).numpy())
         image = Image.fromarray(flow_img)
+        image = image.resize((original_resolution['width'], original_resolution['height']), resample=Image.Resampling.BOX)
         image.save('{}/flow_{:04}_to_{:04}.png'.format(vis_dir, idx+2, idx+3))
     
     # Backward Flow
-    for idx in range(N//2, N):
-        flow_img = flow_viz.flow_to_image(flow_pre[idx].permute(1, 2, 0).numpy())
-        image = Image.fromarray(flow_img)
-        image.save('{}/flow_{:04}_to_{:04}.png'.format(vis_dir, idx-N//2+2, idx-N//2+1))
+    # for idx in range(N//2, N):
+    #     flow_img = flow_viz.flow_to_image(flow_pre[idx].permute(1, 2, 0).numpy())
+    #     image = Image.fromarray(flow_img)
+    #     image = image.resize((original_resolution['width'], original_resolution['height']), resample=Image.Resampling.BOX)
+    #     image.save('{}/flow_{:04}_to_{:04}.png'.format(vis_dir, idx-N//2+2, idx-N//2+1))
 
 @torch.no_grad()
-def inference(model, cfg, resize_factor = 1):
+def inference(model, cfg):
 
     model.eval()
 
-    input_images = prepare_image(cfg, resize_factor)
+    input_images = prepare_image(cfg)
     input_images = input_images[None].cuda()
     padder = InputPadder(input_images.shape)
     input_images = padder.pad(input_images)
@@ -86,6 +99,7 @@ if __name__ == '__main__':
 
     cfg = get_cfg()
     cfg.update(vars(args))
+    original_resolution = prepare_resizing(cfg, args.resize_factor)
 
     model = torch.nn.DataParallel(build_network(cfg))
     model.load_state_dict(torch.load(cfg.model))
@@ -96,6 +110,7 @@ if __name__ == '__main__':
     print(cfg.model)
     print("Parameter Count: %d" % count_parameters(model))
     
-    flow_pre = inference(model.module, cfg, args.resize_factor)
+    
+    flow_pre = inference(model.module, cfg)
             
-    vis_pre(flow_pre, cfg.vis_dir)
+    vis_pre(flow_pre, cfg.vis_dir, original_resolution)
